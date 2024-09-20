c09d830c1ba68cb1f9502d2630924f4d8e52e1f8
MoayedAljaddawi <moayyed1425@gmail.com>
2024-09-20 21:38:04 +0300

    change 20 to 30 in randomizer

--- StudentRandomizer.java
+++ StudentRandomizer.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class StudentRandomizer {
   public List<String> randomizeStudent() {
-    int randomLength = (int) (Math.random() * 20);
+    int randomLength = (int) (Math.random() * 30);
     List<String> randomStudents = new ArrayList<>();
     for (int i = 0; i < randomLength; i++) {
       randomStudents.add("Student " + i);
